e911217b3332e2dad033bc3c3668c615ae992f3b
Damien S <damien.sullins@icloud.com>
2023-07-05 19:14:47 -0700

    change distribution.json to a custom one

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://gist.githubusercontent.com/heyimocto/f915282437c3f303577344e3276594d3/raw/d9db60891747f16921817a812152d3f8c8c7df0b/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
